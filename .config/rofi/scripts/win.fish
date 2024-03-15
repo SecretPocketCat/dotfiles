74f77451f250e498cec5c248fe9c7dd6cd8c7e7a
Jiří H. <jiri@bacehub.de>
2024-03-15 22:27:01 +0100

    update rofi style

--- .config/rofi/scripts/win.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/fish
-
-if [ $argv[1] ]
-    # switch focus
-    hyprctl dispatch focuswindow address:$ROFI_INFO >/dev/null &
-else
-    # parse valid windows into options for rofi
-    hyprctl clients -j | jq -r '.[] | select(.pid != -1) | "  \(.class | clampString(20))  \(.title)\u0000info\u001f\(.address)\u001ficon\u001f\(.class | ascii_downcase)"'
-end
